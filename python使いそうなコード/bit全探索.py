7634f9d24e5cd3f9ea1c311f761407ad1fddc407
izutti <sozyo.snipershot@gmail.com>
2021-04-24 12:29:13 +0900

    bit全探索更新

--- python使いそうなコード/bit全探索.py
+++ python使いそうなコード/bit全探索.py
@@ -14,12 +14,12 @@ for i in range(2**N):
   nX=0
   for j in range(N):
     if ((i >> j) & 1): 
-      money+=CA[j][0]  #金額の更新
+      money+=CA[j][0] #金額の更新
       for k in range(M):
-        U[k]+=CA[j][k+1]　#理解度の更新
+        U[k]+=CA[j][k+1] #理解度の更新
         if U[k]>=X:
-          bU[k]=True    #理解度の閾値チェック
+          bU[k]=True  #理解度の閾値チェック
   if bU.count(True)>=M:
     if (minMoney>money) or (minMoney==-1):
-      minMoney=money　　#最小値更新
+      minMoney=money  #最小値更新
 print(minMoney)
